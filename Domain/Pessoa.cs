fa048602c7066f18c3227e4a78c9d8726354ec1f
deguto <deguto@hotmail.com>
2020-03-24 20:59:19 -0300

    inicial

---
+++ Domain/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+
+    public enum EPessoa
+    {
+        [StringValue("Aqui é o atributo para enum NOME!")]
+        nome = 1,
+        [StringValue("Atributo para enum Cidade!")]
+        cidade = 2
+
+    }
+}
---
+++ Domain/StringValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class StringValue : Attribute
+    {
+        public string Value { get; private set; }
+        public StringValue(string value)
+        {
+            Value = value;
+        }
+    }
+}
